c13d3118cad49aef9edd44b535c157c46046d7c3
Kanglan Tang <47437066+kanglant@users.noreply.github.com>
2025-08-05 16:48:59 -0700

    Modify seed-env pyproject.toml template for maxtext release (#67)

--- python_seed_env/src/seed_env/utils.py
+++ python_seed_env/src/seed_env/utils.py
@@ -76,13 +76,40 @@ def generate_minimal_pyproject_toml(
 
   # TODO: Pass the version as an argument
   # For now, we use a fixed version "0.1.0" as a placeholder.
+  # TODO: Remove the hardcoded part for maxtext in the furture.
   content = f"""\
+# Copyright 2025 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+[build-system]
+requires = ["setuptools"]
+build-backend = "setuptools.build_meta"
+
 [project]
-name = "{project_name}-meta"
+name = "{project_name}"
 version = "0.1.0"
+description = "{project_name} is a pure Python package"
+readme = "README.md"
+license-files = ["LICENSE"]
 requires-python = "=={python_version}.*"
 dependencies = [
 ]
+
+[tool.setuptools.packages.find]
+where = ["{project_name}"]
+include = ["{project_name}*"]
+exclude = ["{project_name}.tests.*"]
 """
   try:
     pyproject_path = os.path.join(output_dir, "pyproject.toml")
