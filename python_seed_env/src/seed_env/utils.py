6b267d28b708d0f252c568ebd29d94b4b457ceb3
Kanglan Tang <47437066+kanglant@users.noreply.github.com>
2025-08-07 10:35:42 -0700

    Fix the seed_env package path (#68)

--- python_seed_env/src/seed_env/utils.py
+++ python_seed_env/src/seed_env/utils.py
@@ -79,13 +79,13 @@ def generate_minimal_pyproject_toml(
   # TODO: Remove the hardcoded part for maxtext in the furture.
   content = f"""\
 # Copyright 2025 Google LLC
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -107,9 +107,10 @@ dependencies = [
 ]
 
 [tool.setuptools.packages.find]
-where = ["{project_name}"]
+where = ["."]
 include = ["{project_name}*"]
-exclude = ["{project_name}.tests.*"]
+# Exclusion will be added later. Commented out here for now to run the maxtext integration tests.
+# exclude = ["{project_name}.tests*"]
 """
   try:
     pyproject_path = os.path.join(output_dir, "pyproject.toml")
